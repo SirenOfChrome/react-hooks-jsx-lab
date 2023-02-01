15b59f0e5a6bd22b4149891e712e26cedfed20e2
Forrest Jones <forrestjones2010@gmail.com>
2023-02-01 16:49:55 -0600

    GDC#11

--- src/components/About.js
+++ src/components/About.js
@@ -1,3 +1,10 @@
+
+
+
+
+
+
+
 import React from "react";
 import { image } from "../data/data";
 
@@ -5,10 +12,12 @@ function About() {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>I am a software developer who loves to code and solve problems.</p>
+      <p>I am a software developer who loves to code and solve problems. I know KungFu like Bruce Lee now.</p>
       <img src={image} alt="I made this" />
     </div>
   );
 }
 
 export default About;
+
+//GDC#11
